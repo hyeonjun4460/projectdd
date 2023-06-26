4a71c03e4368b03ceaea1cf83a735629fd8f8d0d
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-06-26 12:23:10 +0900

    🚚 api, libs에 대한 경로 tsconfig에 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './api/user/user.module';
+import { UserModule } from '@api/user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DatabaseConfigService } from './libs/config/db/database.config';
+import { DatabaseConfigService } from '@libs/config/db/database.config';
 @Module({
   imports: [
     UserModule,
