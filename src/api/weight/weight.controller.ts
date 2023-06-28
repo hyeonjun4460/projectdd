0877faaf965f0cd4700793796c61e276474063d2
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-06-28 15:24:54 +0900

    🛂 weight module에 guard 사용 위해 jwt module 주입 및 컨트롤러에서 이용

--- src/api/weight/weight.controller.ts
+++ src/api/weight/weight.controller.ts
@@ -1,18 +1,22 @@
+import { AuthGuard } from '@api/user/guard/auth.guard';
 import { RegisterWeightBodyDto } from './dto/register-weight.body.dto';
 import { registerWeighParamtDto } from './dto/register-weight.params.dto';
 import { WeightService } from './weight.service';
-import { Controller, Param, Post, Body } from '@nestjs/common';
+import { Controller, Param, Post, Body, UseGuards } from '@nestjs/common';
+import { User } from '@api/user/decorator/user.decorator';
 
 @Controller('weight')
 export class WeightController {
   constructor(private readonly service: WeightService) {}
 
+  @UseGuards(AuthGuard)
   @Post('/:year/:month/:day')
   registerWeight(
     @Param() params: registerWeighParamtDto,
     @Body() body: RegisterWeightBodyDto,
+    @User() user: number,
   ) {
-    return { params, body };
+    return { user, params, body };
     // body로 데이터 받기
     // 유저 토큰 받기(guards)
     // 유저 데코레이터로 토큰 체크하기
