0e302e57b277ad279c8732baa8ab7011e603f15f
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-06-28 15:36:48 +0900

    🛂 guard에서 return하는 값 형태 변경

--- src/api/user/decorator/user.decorator.ts
+++ src/api/user/decorator/user.decorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const User = createParamDecorator(
-  (data: never, context: ExecutionContext): number => {
+  (data: never, context: ExecutionContext): { userId: number } => {
     const response = context.switchToHttp().getResponse();
     return response.locals.user;
   },
