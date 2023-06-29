fce6152b7cc695bd2e8006ac3b34593993af4cd4
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-06-29 16:15:06 +0900

    🚧 create diet param body validation 추가

---
+++ src/api/diet/dto/create.diet.body.dto.ts
@@ -0,0 +1,28 @@
+import { IsBoolean, IsNumber, IsString, Max, Min } from 'class-validator';
+export class CreateDietBodyDto {
+  @IsString()
+  foodName: string;
+
+  @IsNumber()
+  @Min(0)
+  foodAmount: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(23)
+  hour: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(60)
+  min: number;
+
+  @IsString()
+  place: string;
+
+  @IsString()
+  impression: string;
+
+  @IsBoolean()
+  have: boolean;
+}
