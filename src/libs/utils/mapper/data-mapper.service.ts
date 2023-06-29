ee2675961bd0b4048164647ca4f49e90cb4d1178
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-06-29 17:54:39 +0900

    🚧 create diet api 1차 구현(duplicate 처리 필요)

--- src/libs/utils/mapper/data-mapper.service.ts
+++ src/libs/utils/mapper/data-mapper.service.ts
@@ -3,12 +3,15 @@ import { Injectable } from '@nestjs/common';
 import { UserEntity } from '@libs/entity/user.entity';
 import { Repository } from 'typeorm';
 import { WeightEntity } from '@libs/entity/weight.entity';
+import { DietEntity } from '@libs/entity/diet.entity';
 @Injectable()
 export class DataMapperService {
   constructor(
     @InjectRepository(UserEntity) private readonly user: Repository<UserEntity>,
     @InjectRepository(WeightEntity)
     private readonly weight: Repository<WeightEntity>,
+    @InjectRepository(DietEntity)
+    private readonly diet: Repository<DietEntity>,
   ) {}
 
   mapUser(data: Partial<UserEntity>): UserEntity {
@@ -17,5 +20,9 @@ export class DataMapperService {
 
   mapWeight(data: Partial<WeightEntity>): WeightEntity {
     return this.weight.create(data);
+  }
+
+  mapDiet(data: Partial<DietEntity>): DietEntity {
+    return this.diet.create(data);
   }
 }
